b5d348f854beeade3a0555d5e8c22c2db89c9cc4
Carlos123-A <128899376+Carlos123-A@users.noreply.github.com>
2024-09-16 00:52:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from dask.distributed import Client
 
 client = Client(":8786")
 
-ratings = dd.read_csv('/data/u.data', delimiter='\t', header=None, names=['user_id', 'item_id', 'rating', 'timestamp'], encoding='latin1')
-items = dd.read_csv('/data/u.item', delimiter='|', header=None, usecols=[0, 1], names=['item_id', 'title'], encoding='latin1')
-users = dd.read_csv('/data/u.user', delimiter='|', header=None, names=['user_id', 'age', 'gender', 'occupation', 'zip_code'], encoding='latin1')
+ratings = dd.read_csv('/data/u.data.csv', delimiter='\t', header=None, names=['user_id', 'item_id', 'rating', 'timestamp'])
+items = dd.read_csv('/data/u.item.csv', delimiter='|', header=None, usecols=[0, 1], names=['item_id', 'title'])
+users = dd.read_csv('/data/u.user.csv', delimiter='|', header=None, names=['user_id', 'age', 'gender', 'occupation', 'zip_code'])
 
 data = ratings.merge(items, on='item_id').merge(users, on='user_id')
 
