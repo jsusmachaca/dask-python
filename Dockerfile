e1f10b7acac100535f31e36e4f7de9f2a85b6c8e
jsusmachaca <falcorgd@gmail.com>
2024-09-24 21:27:01 -0500

    jaja

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
-FROM python:3.9-slim
+FROM python:3.12-slim
 
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
-COPY app.py app.py
+
+COPY app.py .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY data /data
+COPY data data
 
 EXPOSE 8000
 
--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 import dask.dataframe as dd
 import pandas as pd
 from dask.distributed import Client
+from os import environ, path, listdir
 
-client = Client(":8786")
+client = Client(f"{environ.get("DASK_IP")}:8786")
 
-ratings = dd.read_csv('/data/u.data', delimiter='\t', header=None, names=['user_id', 'item_id', 'rating', 'timestamp'], encoding='ISO-8859-1')
-items = dd.read_csv('/data/u.item', delimiter='|', header=None, usecols=[0, 1], names=['item_id', 'title'], encoding='ISO-8859-1')
-users = dd.read_csv('/data/u.user', delimiter='|', header=None, names=['user_id', 'age', 'gender', 'occupation', 'zip_code'], encoding='ISO-8859-1')
+print(listdir('.'))
+print(listdir('data'))
 
-data = ratings.merge(items, on='item_id').merge(users, on='user_id')
+try:
+    ratings = dd.read_csv('data/u.data.csv', delimiter='\t', header=None, names=['user_id', 'item_id', 'rating', 'timestamp'], encoding='UTF-8')
+    items = dd.read_csv('data/u.item.csv', delimiter='|', header=None, usecols=[0, 1], names=['item_id', 'title'], encoding='ISO-8859-1')
+    users = dd.read_csv('data/u.user.csv', delimiter='|', header=None, names=['user_id', 'age', 'gender', 'occupation', 'zip_code'], encoding='ISO-8859-1')
 
-rating_avg = data.groupby('item_id')['rating'].mean().compute()
+    data = ratings.merge(items, on='item_id').merge(users, on='user_id')
 
-top_movies = rating_avg.sort_values(ascending=False).head(10).index
+    rating_avg = data.groupby('item_id')['rating'].mean().compute()
 
-top_movies_data = data[data['item_id'].isin(top_movies)]
+    top_movies = rating_avg.sort_values(ascending=False).head(10).index
 
-occupation_ratings = top_movies_data.groupby(['occupation', 'title'])['rating'].mean().compute()
+    top_movies_data = data[data['item_id'].isin(top_movies)]
 
-print("Películas con más votaciones:")
-print(rating_avg.sort_values(ascending=False).head(10))
+    occupation_ratings = top_movies_data.groupby(['occupation', 'title'])['rating'].mean().compute()
 
-print("\nRanking de ocupaciones para las películas top:")
-print(occupation_ratings)
+    print("Películas con más votaciones:")
+    print(rating_avg.sort_values(ascending=False).head(10))
+
+    print("\nRanking de ocupaciones para las películas top:")
+    print(occupation_ratings)
+except Exception as e:
+    print(e)
